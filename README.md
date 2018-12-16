c35bd670cdd0ad516066e533a463a1f3116e22cc
babar7 <babarshair97@gmail.com>
2018-12-17 02:36:09 +0500

    Redux Basics With React-Native

---
+++ README.md
@@ -0,0 +1,2 @@
+# react-native-login-signup-boiler-01
+# basics-of-redux-with-react-native
---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers';
+import { Header } from './component/common';
+import LibraryList from './component/LibraryList';
+
+const App = () => {
+  return (
+    <Provider store={createStore(reducers)}>
+      <View style={{ flex: 1 }}>
+        <Header headerText="Tech Stack" />
+        <LibraryList />
+      </View>
+    </Provider>
+  );
+};
+
+export default App;
